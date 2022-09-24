b4e044c5d5e1c843048fa953f4b06c497ddd1237
viedamayanti <vinaniekdamayanti@gmail.com>
2022-09-24 09:27:29 +0200

    nodejs-week2-mealsharing

--- src/backend/api/meals.js
+++ src/backend/api/meals.js
@@ -2,47 +2,116 @@ const express = require('express');
 const router = express.Router();
 const knex = require('../database');
 
-router.get('/', async (request, response) => {
+router.get('/', async (req, res) => {
   try {
-    // knex syntax for selecting things. Look up the documentation for knex for further info
-    const titles = await knex('meals').select('title');
-    response.json(titles);
+    const result = await knex.raw(`SELECT * FROM meal`);
+    res.json({
+      meals: result[0],
+    });
   } catch (error) {
-    throw error;
+    res.status(500).json({ Msg: 'Internal server error' });
   }
 });
+
 router.get('/future-meals', async (req, res) => {
-  const result = await knex.raw(
-    `SELECT * FROM meal ORDER BY created_date DESC `
-  );
-  return res.json({
-    meals: result[0],
-  });
+  try {
+    const result = await knex.raw(
+      `SELECT * FROM meal ORDER BY created_date DESC `
+    );
+    res.json({
+      meals: result[0],
+    });
+  } catch {
+    res.status(500).json({ Msg: 'Internal server error' });
+  }
 });
+
 router.get('/past-meals', async (req, res) => {
-  const result = await knex.raw(`SELECT * FROM meal ORDER BY created_date ASC`);
-  return res.json({
-    meals: result[0],
-  });
+  try {
+    const result = await knex.raw(
+      `SELECT * FROM meal ORDER BY created_date ASC`
+    );
+    res.json({
+      meals: result[0],
+    });
+  } catch {
+    res.status(500).json({ Msg: 'Internal server error' });
+  }
 });
 
 router.get('/all-meals', async (req, res) => {
-  const result = await knex.raw(`SELECT id, title FROM meal`);
-  return res.json({
-    meals: result[0],
-  });
+  try {
+    const result = await knex.raw(`SELECT id, title FROM meal`);
+    res.json({
+      meals: result[0],
+    });
+  } catch {
+    res.status(500).json({ Msg: 'Internal server error' });
+  }
 });
 router.get('/first-meals', async (req, res) => {
-  const result = await knex.raw(`SELECT * FROM meal ORDER BY id LIMIT 1`);
-  return res.json({
-    meals: result[0],
-  });
+  try {
+    const result = await knex.raw(`SELECT * FROM meal ORDER BY id LIMIT 1`);
+    return res.json({
+      meals: result[0],
+    });
+  } catch {
+    res.status(500).json({ Msg: 'Internal server error' });
+  }
 });
 router.get('/last-meals', async (req, res) => {
-  const result = await knex.raw(`SELECT * FROM meal ORDER BY id DESC LIMIT 1`);
-  return res.json({
-    meals: result[0],
-  });
+  try {
+    const result = await knex.raw(
+      `SELECT * FROM meal ORDER BY id DESC LIMIT 1`
+    );
+    return res.json({
+      meals: result[0],
+    });
+  } catch {
+    res.status(500).json({ Msg: 'Internal server error' });
+  }
+});
+
+router.post('/', async (req, res) => {
+  try {
+    const result = await knex.insert(req.body).into('meal');
+    res.send(`ID number ${result} is created`);
+  } catch (error) {
+    res.status(500).json({ Msg: 'Internal server error' });
+  }
+});
+
+router.get('/:id', async (req, res) => {
+  try {
+    const paramsValue = req.params.id;
+    const result = await knex('meal').where({ id: paramsValue });
+    res.send(result);
+  } catch (error) {
+    res.status(500).json({ Msg: 'Internal server error' });
+  }
+});
+
+//Update without hardcore
+router.put('/:id', async (req, res) => {
+  try {
+    const paramsValue = req.params.id;
+    const result = await knex('meal')
+      .where({ id: paramsValue })
+      .update({ price: 80 });
+    res.send('Data is updated');
+  } catch (error) {
+    res.status(500).json({ Msg: 'Internal server error' });
+  }
+});
+
+router.delete('/:id', async (req, res) => {
+  try {
+    const paramsValue = req.params.id;
+    const result = await knex('meal').where({ id: paramsValue }).del();
+    return res.send('Deleted meal');
+  } catch (error) {
+    res.status(500).json({ Msg: 'Internal server error' });
+  }
 });
 
 module.exports = router;
--- src/backend/app.js
+++ src/backend/app.js
@@ -5,6 +5,7 @@ const router = express.Router();
 const path = require('path');
 
 const mealsRouter = require('./api/meals');
+const reservationRouter = require('./api/reservations');
 const buildPath = path.join(__dirname, '../../dist');
 const port = process.env.PORT || 5000;
 const cors = require('cors');
@@ -25,64 +26,13 @@ app.get('/my-route', (req, res) => {
   res.send('Hi friend');
 });
 
-app.get('/future-meals', async (req, res) => {
-  try {
-    const [rows] = await knex.raw(
-      'SELECT * FROM meal ORDER BY created_date DESC'
-    );
-    res.json(rows);
-  } catch (e) {
-    console.error(e);
-    res.status(400).json({ error: 'Internal server error' });
-  }
-});
-app.get('/past-meals', async (req, res) => {
-  try {
-    const [rows] = await knex.raw(
-      'SELECT * FROM meal ORDER BY created_date ASC'
-    );
-    res.json(rows);
-  } catch (e) {
-    console.error(e);
-    res.status(400).json({ error: 'Internal server error' });
-  }
-});
-app.get('/all-meals', async (req, res) => {
-  try {
-    const [rows] = await knex.raw('SELECT id, title FROM meal');
-    res.json(rows);
-  } catch (e) {
-    console.error(e);
-    res.status(400).json({ error: 'Internal server error' });
-  }
-});
-app.get('/first-meals', async (req, res) => {
-  try {
-    const [rows] = await knex.raw('SELECT * FROM meal ORDER BY id LIMIT 1');
-    res.json(rows);
-  } catch (e) {
-    console.error(e);
-    res.status(400).json({ error: 'Internal server error' });
-  }
-});
-app.get('/last-meals', async (req, res) => {
-  try {
-    const [rows] = await knex.raw(
-      'SELECT * FROM meal ORDER BY id DESC LIMIT 1'
-    );
-    res.json(rows);
-  } catch (e) {
-    console.error(e);
-    res.status(400).json({ error: 'Internal server error' });
-  }
-});
+router.use('/reservations', reservationRouter);
 
 if (process.env.API_PATH) {
   app.use(process.env.API_PATH, router);
 } else {
   throw 'API_PATH is not set. Remember to set it in your .env file';
 }
-
 // for the frontend. Will first be covered in the react class
 app.use('*', (req, res) => {
   res.sendFile(path.join(`${buildPath}/index.html`));
